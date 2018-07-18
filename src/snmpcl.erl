147fbf508a9e066fe448ab977c1b5fceacfb5b61
Artem Teslenko <ates@ipv6.dp.ua>
2018-07-18 13:41:03 +0300

    Add walk_next

--- src/snmpcl.erl
+++ src/snmpcl.erl
@@ -47,10 +47,28 @@ walk(Version, Address, Community, Oid) ->
             snmpm:unregister_agent(snmpcl_user, "snmpcl_user")
     end,
     snmpm:register_agent(snmpcl_user, "snmpcl_user", Options),
+
     case snmpm:sync_get(snmpcl_user, "snmpcl_user", [Oid]) of
         {ok,  {_, _, Result}, _Remaining} ->
             [{_, Oid, Type, Value, _}] = Result,
-            {Oid, Type, Value};
+            walk_next(Oid, [{Oid, Type, Value}]);
         {error, Reason} ->
             error_logger:error_msg("Can't send request to ~p due to ~p~n", [Address, Reason])
+    end.
+
+walk_next(Oid, Acc) ->
+    walk_next(Oid, lists:nth(length(Oid) - 1, Oid), Acc).
+
+walk_next(BaseOid, Index, Acc) ->
+    case snmpm:sync_get_next(snmpcl_user, "snmpcl_user", [BaseOid]) of
+        {ok,  {_, _, Result}, _Remaining} ->
+            [{_, Oid, Type, Value, _}] = Result,
+            case lists:nth(length(Oid) - 1, Oid) =:= Index of
+                true ->
+                    walk_next(Oid, Index, [{Oid, Type, Value} | Acc]);
+                false ->
+                    Acc
+            end;
+        {error, Reason} ->
+            error_logger:error_msg("Can't send request, reason: ~p~n", [Reason])
     end.
